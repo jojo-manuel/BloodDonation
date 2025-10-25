d4910188984c52c50988d75e34e94f738db970ab
Jojo Manuel P <jojomanuelp543@gmail.com>
2025-10-25 10:30:54 +0000

    Oct 25, 2025, 4:00 PM

--- backend/Route/userRoutes.js
+++ backend/Route/userRoutes.js
@@ -37,7 +37,7 @@ const path = require('path');
 
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'backend/uploads/');
+    cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
     const ext = path.extname(file.originalname);
